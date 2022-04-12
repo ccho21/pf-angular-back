ec5af19632cdce2e9712687f35177c5f315701b7
Charles Cho <changsoo.charles.cho@gmail.com>
2022-04-12 17:20:00 -0400

    added a get user by id

--- routes/api/users.js
+++ routes/api/users.js
@@ -4,9 +4,23 @@ const gravatar = require('gravatar');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const config = require('config');
+const auth = require('../../middleware/auth');
 const { check, validationResult } = require('express-validator');
 
 const User = require('../../models/User');
+
+//  @route      GET api/auth
+//  @desc       GET User Detail
+//  @access     Public
+router.get('/:id', auth, async (req, res) => {
+  try {
+    const user = await User.findById(req.user.id).select('-password');
+    res.json(user);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
+});
 
 //  @route      POST api/users/sign-up
 //  @desc       Register user
